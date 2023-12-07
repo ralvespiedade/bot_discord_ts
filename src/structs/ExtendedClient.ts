4123c203b284aa50b926ccf1550ac189bfee2c50
Ricardo Alves <ralvespiedade@gmail.com>
2023-12-07 09:45:13 -0300

    configurando o hadler de comandos

--- src/structs/ExtendedClient.ts
+++ src/structs/ExtendedClient.ts
@@ -1,5 +1,8 @@
-import { Client, Collection, Partials, IntentsBitField, BitFieldResolvable, GatewayIntentsString } from "discord.js";
+import { Client, Collection, Partials, IntentsBitField, BitFieldResolvable, GatewayIntentsString, ApplicationCommandDataResolvable } from "discord.js";
 import dotenv from "dotenv";
+import fs from "fs";
+import path from "path";
+
 import { CommandType, ComponentsButton, ComponentsSelect, ComponentsModal } from "./types/Commands";
 dotenv.config();
 
@@ -22,9 +25,50 @@ export class ExtendedClient extends Client {
       ]
     })
   }
+
+
   public start() {
+    this.registerModules();
     this.login(process.env.BOT_TOKEN);
   }
 
-  //private registerCommands(commands: Array)
+  private registerCommands(commands: Array<ApplicationCommandDataResolvable>) {
+    this.application?.commands.set(commands)
+    .then(() => {
+      console.log("✅ Slash commands (/) defined".green);
+
+    })
+    .catch(error => {
+      console.log(`❌ an error occurred while trying to set the Slash Commands (/): /n${error}`.red)
+    })
+      
+  }
+
+  private registerModules() {
+    //cria o array de comandos
+    const slashCommands: Array<ApplicationCommandDataResolvable> = new Array();
+    
+    const commandsPath = path.join(__dirname, "..", "commands");
+    const fileCondition = (fileName: string) => fileName.endsWith(".ts") || fileName.endsWith(".js");
+    
+    fs.readdirSync(commandsPath).forEach(local => {
+      fs.readdirSync(commandsPath + `/${local}/`).filter(fileCondition).forEach(async fileName => {
+
+        const command: CommandType = (await import(`../commands/${local}/${fileName}`))?.default;
+        const { name, buttons, selects, modals } = command
+
+        if (name) {
+          this.commands.set(name, command);
+          slashCommands.push(command);
+
+          if (buttons) buttons.forEach((run, key) => this.buttons.set(key, run));
+          if (selects) selects.forEach((run, key) => this.selects.set(key, run));
+          if (modals) modals.forEach((run, key) => this.modals.set(key, run));
+        }
+      })
+    });
+
+    this.on("ready", () => this.registerCommands(slashCommands))
+
+  }
 }
