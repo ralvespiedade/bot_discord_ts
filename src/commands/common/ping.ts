ccff65ba796913a39e1101b35b48fe3736f84629
Ricardo Alves <ralvespiedade@gmail.com>
2023-12-12 10:49:02 -0300

    adicionando solicitacao de cpf

--- src/commands/common/ping.ts
+++ src/commands/common/ping.ts
@@ -1,17 +1,26 @@
-import { ActionRowBuilder, ApplicationCommandType, ButtonBuilder, ButtonStyle, Collection } from "discord.js";
+import { ActionRowBuilder, ApplicationCommandOptionType, ApplicationCommandType, ButtonBuilder, ButtonStyle, Collection } from "discord.js";
 import { Command } from "../../structs/types/Commands";
 
 export default new Command({
-  name: "ping",
+  name: "gerenciar",
   description: "reply with pong",
   type: ApplicationCommandType.ChatInput,
-  run({interaction}) {
-    const row = new ActionRowBuilder<ButtonBuilder>({components: [
+  options: [{
+    name: "cpf",
+    description: "Digite seu CPF: ",
+    type: ApplicationCommandOptionType.String,
+    required: true
+  }],
+  async run({interaction, options}) {
+    /* const row = new ActionRowBuilder<ButtonBuilder>({components: [
       new ButtonBuilder({custom_id: "test-button", label: "Clique aqui", style: ButtonStyle.Success})
-    ]})
+    ]}) */
+    
+    //true para garantir que vai retornar uma string, se for uma oção obrigatória
+    const texto = options.getString("texto", true)
 
 
-    interaction.reply({ephemeral: true, content: "pong", components: [row]})
+    await interaction.reply({content: `Seu CPF é: ${texto}`})
   },
   buttons: new Collection([
     ["test-button", async (interaction) => {
